b23b58032c4c629e5ba7be418f86ef75fbc44a48
SaloniPardeshi1412 <saloni.p@binyuga.com>
2024-04-03 13:15:27 +0530

    login api called successfully.

--- lib/app/services/api/log_in/log_in_data.dart
+++ lib/app/services/api/log_in/log_in_data.dart
@@ -1,17 +1,21 @@
-class LoginRequestData {
-  String username;
-  String password;
-
-  LoginRequestData({
-    required this.username,
-    required this.password,
-  });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'username': username,
-      'password': password,
-    };
-  }
-}
-
+// class LoginRequestData {
+//   String username;
+//   String password;
+//
+//   LoginRequestData({
+//     required this.username,
+//     required this.password,
+//   });
+//
+//   LoginRequestData.fromJson(
+//       Map<String, dynamic> json,
+//       ) : username = json["username"] as String ,
+//         password = json["password"] as String;
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'username': username,
+//       'password': password,
+//     };
+//   }
+// }
