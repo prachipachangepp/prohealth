2e8073bc34460d36d0503e273763e7a05bdfd4d9
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2024-04-08 15:46:28 +0530

    log in api added get otp api added verify api added

--- lib/app/services/api/log_in/log_in_data.dart
+++ lib/app/services/api/log_in/log_in_data.dart
@@ -1,21 +1,19 @@
-// class LoginRequestData {
-//   String username;
-//   String password;
-//
-//   LoginRequestData({
-//     required this.username,
-//     required this.password,
-//   });
-//
-//   LoginRequestData.fromJson(
-//       Map<String, dynamic> json,
-//       ) : username = json["username"] as String ,
-//         password = json["password"] as String;
-//
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'username': username,
-//       'password': password,
-//     };
-//   }
-// }
+class LoginRequestData {
+  String username;
+  String password;
+
+  LoginRequestData({
+    required this.username,
+    required this.password,
+  });
+
+  LoginRequestData.fromJson(
+    Map<String, dynamic> json,
+  )   : username = json["username"] as String,
+        password = json["password"] as String;
+
+  Map<String, dynamic> toJson() => {
+        'username': username,
+        'password': password,
+      };
+}
