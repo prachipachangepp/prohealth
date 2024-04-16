c726bf1fe2cbdac4fdd0648280e01ea6bd07ad79
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2024-04-16 15:47:50 +0530

    pageview added successfully to responsive screen sm

--- lib/presentation/screens/sm_module/responsive_screen_sm.dart
+++ lib/presentation/screens/sm_module/responsive_screen_sm.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:prohealth/presentation/screens/sm_module/app_bar_sm.dart';
+import 'package:prohealth/presentation/screens/hr_module/manage/widgets/custom_icon_button_constant.dart';
 
 class ResponsiveScreenSM extends StatelessWidget {
   @override
@@ -7,8 +7,10 @@ class ResponsiveScreenSM extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
         if (constraints.maxWidth >= 850) {
+          // Render SMDesktop screen for large screens
           return SMDesktop();
         } else {
+          // Render SMTablet screen for small screens
           return SMTablet();
         }
       },
@@ -17,9 +19,122 @@ class ResponsiveScreenSM extends StatelessWidget {
 }
 
 class SMDesktop extends StatelessWidget {
+  final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return AppBarSM();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
+            height: 131,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff000000).withOpacity(0.25),
+                  blurRadius: 4,
+                  offset: Offset(0, 4),
+                ),
+              ],
+              color: Color(0xffFFFFFF),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 20,
+                        // color: Colors.grey,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 10,
+                      child: Container(
+                        // color: Colors.red,
+                        height: 20,
+                      ),
+                    ),
+                  ],
+                ),
+
+                /// buttons
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        // color: Colors.yellow,
+                        height: 40,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            CustomButton(
+                              height: 30,
+                              width: 100,
+                              onPressed: () {
+                                if (_pageController.page != 0) {
+                                  _pageController.animateToPage(0,
+                                      duration: Duration(milliseconds: 500),
+                                      curve: Curves.ease);
+                                }
+                              },
+                              text: 'Button 1',
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            CustomButton(
+                              height: 30,
+                              width: 100,
+                              onPressed: () {
+                                if (_pageController.page != 1) {
+                                  _pageController.animateToPage(1,
+                                      duration: Duration(milliseconds: 500),
+                                      curve: Curves.ease);
+                                }
+                              },
+                              text: 'Button 2',
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            CustomButton(
+                              height: 30,
+                              width: 100,
+                              onPressed: () {
+                                if (_pageController.page != 2) {
+                                  _pageController.animateToPage(2,
+                                      duration: Duration(milliseconds: 500),
+                                      curve: Curves.ease);
+                                }
+                              },
+                              text: 'Button 3',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: PageView(
+            controller: _pageController,
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              Container(color: Colors.blue),
+              Container(color: Colors.green),
+              Container(color: Colors.orange),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
