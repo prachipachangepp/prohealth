8e824b752a4609d1480f2c19509e09a0264c6d11
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2024-04-16 15:13:10 +0530

    pageview added successfully to responsive screen sm

--- lib/presentation/screens/sm_module/app_bar_sm.dart
+++ lib/presentation/screens/sm_module/app_bar_sm.dart
@@ -1,64 +1,77 @@
-import 'package:flutter/material.dart';
-
-class AppBarSM extends StatelessWidget {
-  const AppBarSM({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Column(
-        children: [
-          ///main container
-          Container(
-            height: 131,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff000000).withOpacity(0.25),
-                  blurRadius: 4,
-                  offset: Offset(0, 4),
-                ),
-              ],
-              color: Color(0xffFFFFFF)
-            ),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                        child: Container(
-                      //color: Colors.grey,
-
-                    )),
-                    Expanded(
-                      flex: 10,
-                        child: Container(
-                         // color: Colors.red,
-                        )),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                        child: Container(
-                      color: Colors.grey,
-
-                    )),
-                    Expanded(
-                      flex: 10,
-                        child: Container(
-                          color: Colors.red,
-                        )),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//
+// class AppBarSM extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(15.0),
+//       child: Container(
+//         height: 131,
+//         decoration: BoxDecoration(
+//           boxShadow: [
+//             BoxShadow(
+//               color: Color(0xff000000).withOpacity(0.25),
+//               blurRadius: 4,
+//               offset: Offset(0, 4),
+//             ),
+//           ],
+//           color: Color(0xffFFFFFF),
+//         ),
+//         child: Column(
+//           children: [
+//             Row(
+//               children: [
+//                 Expanded(
+//                   flex: 2,
+//                   child: Container(
+//                     height: 20,
+//                     color: Colors.grey,
+//                   ),
+//                 ),
+//                 Expanded(
+//                   flex: 10,
+//                   child: Container(
+//                     color: Colors.red,
+//                     height: 20,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             Row(
+//               children: [
+//                 Expanded(
+//                   child: Container(
+//                     color: Colors.yellow,
+//                     height: 40,
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         CustomButton(
+//                           height: 30,
+//                           width: 100,
+//                           onPressed: () {},
+//                           text: 'Button 1',
+//                         ),
+//                         CustomButton(
+//                           height: 30,
+//                           width: 100,
+//                           onPressed: () {},
+//                           text: 'Button 2',
+//                         ),
+//                         CustomButton(
+//                           height: 30,
+//                           width: 100,
+//                           onPressed: () {},
+//                           text: 'Button 3',
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
