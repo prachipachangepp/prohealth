05b3fee3233e0fe02b06d936131aed87eded0d36
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2024-05-14 17:55:36 +0530

    log in screen base updated

--- lib/presentation/screens/sm_module/hr_screens/hr_sales.dart
+++ lib/presentation/screens/sm_module/hr_screens/hr_sales.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:prohealth/app/resources/color.dart';
@@ -7,8 +8,6 @@ import 'package:prohealth/presentation/screens/sm_module/hr_screens/widgets/add_
 import 'package:prohealth/presentation/screens/sm_module/hr_screens/widgets/edit_emp_popup_const.dart';
 import 'package:prohealth/presentation/screens/sm_module/widgets/table_constant.dart';
 import 'package:prohealth/presentation/widgets/custom_icon_button_constant.dart';
-
-import '../../../../app/resources/font_manager.dart';
 
 class HrSalesScreen extends StatefulWidget {
   HrSalesScreen({super.key});
@@ -35,6 +34,96 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
     itemsPerPage = 6;
     items = List.generate(20, (index) => 'Item ${index + 1}');
   }
+
+  ///
+  List<Color> _selectedColors = List.filled(
+      2, Color(0xffE8A87D)); // Initialize with default color for 2 items
+  List<String> _selectedColorCodes =
+      List.filled(2, ''); // Initialize with empty strings for 2 items
+
+  void _openColorPicker(int index) async {
+    Color? pickedColor = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Pick a color'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ColorPicker(
+                  borderColor: _selectedColors[index],
+                  onColorChanged: (Color color) {
+                    setState(() {
+                      _selectedColors[index] = color;
+                      _selectedColorCodes[index] =
+                          color.value.toRadixString(16).toUpperCase();
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(_selectedColors[index]);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (pickedColor != null) {
+      setState(() {
+        _selectedColors[index] = pickedColor;
+        _selectedColorCodes[index] =
+            pickedColor.value.toRadixString(16).toUpperCase();
+      });
+    }
+  }
+
+  ///
+  // Color _selectedColor = Color(0xffE8A87D); // Default color
+  // void _openColorPicker() async {
+  //   Color? pickedColor = await showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return AlertDialog(
+  //         title: Text('Pick a color'),
+  //         content: SingleChildScrollView(
+  //           child: ColorPicker(
+  //             // The initial color selected in the picker
+  //             borderColor: _selectedColor,
+  //
+  //             // Function that gets called whenever the picked color changes
+  //             onColorChanged: (Color color) {
+  //               setState(() {
+  //                 _selectedColor = color;
+  //               });
+  //             },
+  //           ),
+  //         ),
+  //         actions: <Widget>[
+  //           TextButton(
+  //             child: Text('OK'),
+  //             onPressed: () {
+  //               Navigator.of(context).pop(_selectedColor);
+  //             },
+  //           ),
+  //         ],
+  //       );
+  //     },
+  //   );
+  //
+  //   if (pickedColor != null) {
+  //     setState(() {
+  //       _selectedColor = pickedColor;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -136,13 +225,16 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
                                 decoration: TextDecoration.none,
                               ),
                             ),
-                            Container(
-                              width: MediaQuery.of(context).size.width / 20,
-                              height: 22,
-                              // margin: EdgeInsets.only(right: 20),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: Color(0xffE8A87D),
+                            GestureDetector(
+                              onTap: () => _openColorPicker(
+                                  index), // Pass index to open color picker for the correct item
+                              child: Container(
+                                width: MediaQuery.of(context).size.width / 20,
+                                height: 22,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: _selectedColors[index],
+                                ),
                               ),
                             ),
                             Row(
@@ -153,20 +245,47 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
                                       context: context,
                                       builder: (BuildContext context) {
                                         return EditPopupWidget(
-                                            typeController: typeController,
-                                            shorthandController:
-                                                shorthandController,
-                                            emailController: emailController,
-                                            containerColor: Color(0xffE8A87D),
-                                            onSavePressed: () {});
+                                          typeController:
+                                              TextEditingController(),
+                                          shorthandController:
+                                              TextEditingController(),
+                                          emailController:
+                                              TextEditingController(),
+                                          containerColor:
+                                              _selectedColors[index],
+                                          onSavePressed: () {},
+                                          onColorChanged: (Color color) {
+                                            // Handle color change here
+                                          },
+                                        );
                                       },
                                     );
                                   },
-                                  icon: Icon(
-                                    Icons.edit_outlined,
-                                    color: ColorManager.mediumgrey,
-                                  ),
+                                  icon: Icon(Icons.edit_outlined),
+                                  color: ColorManager.mediumgrey,
                                 ),
+                                // IconButton(
+                                //   onPressed: () {
+                                //     showDialog(
+                                //       context: context,
+                                //       builder: (BuildContext context) {
+                                //         return EditPopupWidget(
+                                //           typeController: typeController,
+                                //           shorthandController:
+                                //               shorthandController,
+                                //           emailController: emailController,
+                                //           containerColor: Color(0xffE8A87D),
+                                //           onSavePressed: () {},
+                                //           onColorChanged: (Color) {},
+                                //         );
+                                //       },
+                                //     );
+                                //   },
+                                //   icon: Icon(
+                                //     Icons.edit_outlined,
+                                //     color: ColorManager.mediumgrey,
+                                //   ),
+                                // ),
                                 IconButton(
                                   onPressed: () {},
                                   icon: Icon(
@@ -188,7 +307,7 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
         Container(
           height: 30,
           // color: Colors.black12,
-          child:  Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
@@ -196,25 +315,25 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
                 height: 20,
                 decoration: BoxDecoration(
                   shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(6.39),
+                  borderRadius: BorderRadius.circular(4),
                   border: Border.all(
-                    color: ColorManager.grey,
-                    width: 0.79,
+                    color: Colors.grey,
+                    width: 1.0,
                   ),
                 ),
                 child: IconButton(
-                  padding: EdgeInsets.only(bottom: 1.5),
+                  padding: EdgeInsets.only(bottom: 2),
                   icon: Icon(Icons.chevron_left),
                   onPressed: () {
                     setState(() {
                       currentPage = currentPage > 1 ? currentPage - 1 : 1;
                     });
                   },
-                  color: ColorManager.black,
+                  color: Colors.black,
                   iconSize: 20,
                 ),
               ),
-              SizedBox(width: 3),
+              SizedBox(width: 3), // Add space between containers
               for (var i = 1; i <= (items.length / itemsPerPage).ceil(); i++)
                 if (i == 1 ||
                     i == currentPage ||
@@ -228,29 +347,22 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
                     child: Container(
                       width: 20,
                       height: 20,
-                      margin: EdgeInsets.only(left: 5,right: 5),
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
                         shape: BoxShape.rectangle,
                         borderRadius: BorderRadius.circular(4),
                         border: Border.all(
-                          color: currentPage == i ? ColorManager.blueprime : ColorManager.grey,
+                          color: currentPage == i ? Colors.blue : Colors.grey,
                           width: currentPage == i ? 2.0 : 1.0,
                         ),
                         color:
-                        currentPage == i ? ColorManager.blueprime : Colors.transparent,
-                        // border: Border.all(
-                        //   color: currentPage == i
-                        //       ? Colors.blue
-                        //       : Colors.transparent,
-                        // ),
-
+                            currentPage == i ? Colors.blue : Colors.transparent,
                       ),
                       child: Text(
                         '$i',
                         style: TextStyle(
                           color: currentPage == i ? Colors.white : Colors.grey,
-                          fontWeight: FontWeightManager.bold,
+                          fontWeight: FontWeight.bold,
                           fontSize: 12,
                         ),
                       ),
@@ -260,21 +372,21 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
                   Text(
                     '..',
                     style: TextStyle(
-                      color: ColorManager.black,
-                      fontWeight: FontWeightManager.bold,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
                       fontSize: 12,
                     ),
                   ),
+              SizedBox(width: 3),
               Container(
                 width: 20,
                 height: 20,
-                alignment: Alignment.center,
                 decoration: BoxDecoration(
                   shape: BoxShape.rectangle,
                   borderRadius: BorderRadius.circular(4),
-                  border:Border.all(
+                  border: Border.all(
                     color: Colors.grey,
-                    width: 0.79,
+                    width: 1.0,
                   ),
                 ),
                 child: IconButton(
@@ -283,12 +395,12 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
                   onPressed: () {
                     setState(() {
                       currentPage =
-                      currentPage < (items.length / itemsPerPage).ceil()
-                          ? currentPage + 1
-                          : (items.length / itemsPerPage).ceil();
+                          currentPage < (items.length / itemsPerPage).ceil()
+                              ? currentPage + 1
+                              : (items.length / itemsPerPage).ceil();
                     });
                   },
-                  color: ColorManager.black,
+                  color: Colors.black,
                   iconSize: 20,
                 ),
               ),
@@ -299,3 +411,6 @@ class _HrSalesScreenState extends State<HrSalesScreen> {
     );
   }
 }
+//add employee popup need table in which we can fetch list  from api
+///Abbrevation
+///Type of employee
--- lib/presentation/screens/sm_module/hr_screens/widgets/edit_emp_popup_const.dart
+++ lib/presentation/screens/sm_module/hr_screens/widgets/edit_emp_popup_const.dart
@@ -1,9 +1,20 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:prohealth/app/resources/color.dart';
 import 'package:prohealth/app/resources/value_manager.dart';
+
 import '../../widgets/button_constant.dart';
 import '../../widgets/text_form_field_const.dart';
+
+class EditPopupController extends GetxController {
+  Rx<Color> containerColor = Colors.red.obs;
+
+  void updateColor(Color newColor) {
+    containerColor.value = newColor;
+  }
+}
 
 class EditPopupWidget extends StatelessWidget {
   final TextEditingController typeController;
@@ -12,12 +23,15 @@ class EditPopupWidget extends StatelessWidget {
   final VoidCallback onSavePressed;
   final Color containerColor;
 
+  final Function(Color) onColorChanged;
+
   EditPopupWidget({
     required this.typeController,
     required this.shorthandController,
     required this.emailController,
     required this.containerColor,
     required this.onSavePressed,
+    required this.onColorChanged,
   });
 
   @override
@@ -46,54 +60,107 @@ class EditPopupWidget extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(
-                  vertical: AppPadding.p3,horizontal: AppPadding.p20),
+                  vertical: AppPadding.p3, horizontal: AppPadding.p20),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     children: [
-                      Text('Color',
-                        style: GoogleFonts.firaSans(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w700,
-                          color: Color(0xff686464),
-                          decoration: TextDecoration.none,
-                        ),),
-                      SizedBox(width: AppSize.s25,),
-                      Container(
-                        padding: EdgeInsets.all(2),
-                        width: 61,
-                        height: 22,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(2),
-                          border: Border.all(width: 1, color: Color(0xffE9E9E9),
-                          ),
-                        ),
+                      Text('Color'),
+                      SizedBox(width: 10),
+                      GestureDetector(
+                        onTap: () {
+                          _openColorPicker(context);
+                        },
                         child: Container(
-                          width: 57,
-                          height: 16,
+                          width: 60,
+                          height: 20,
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(2),
                             color: containerColor,
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            ),
                           ),
                         ),
-                      )
-
+                      ),
                     ],
                   ),
-                  SizedBox(height: AppSize.s16,),
-                  SMTextFConst(controller: typeController, keyboardType: TextInputType.text, text: 'Type',),
-                  SizedBox(height: AppSize.s16,),
-                  SMTextFConst(controller: shorthandController, keyboardType: TextInputType.streetAddress, text: 'Shorthand',),
-                  SizedBox(height: AppSize.s16,),
-                  SMTextFConst(controller: emailController, keyboardType: TextInputType.emailAddress, text: 'Type of Employee',),
-                  SizedBox(height: MediaQuery.of(context).size.width/27,),
+                  // Row(
+                  //   children: [
+                  //     Text(
+                  //       'Color',
+                  //       style: GoogleFonts.firaSans(
+                  //         fontSize: 12,
+                  //         fontWeight: FontWeight.w700,
+                  //         color: Color(0xff686464),
+                  //         decoration: TextDecoration.none,
+                  //       ),
+                  //     ),
+                  //     SizedBox(
+                  //       width: AppSize.s25,
+                  //     ),
+                  //     GestureDetector(
+                  //       onTap: () {
+                  //         onColorChanged(containerColor);
+                  //       },
+                  //       child: Container(
+                  //         padding: EdgeInsets.all(2),
+                  //         width: 61,
+                  //         height: 22,
+                  //         decoration: BoxDecoration(
+                  //           borderRadius: BorderRadius.circular(2),
+                  //           border: Border.all(
+                  //             width: 1,
+                  //             color: Color(0xffE9E9E9),
+                  //           ),
+                  //         ),
+                  //         child: Container(
+                  //           width: 57,
+                  //           height: 16,
+                  //           decoration: BoxDecoration(
+                  //             borderRadius: BorderRadius.circular(2),
+                  //             color: containerColor,
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                  SizedBox(
+                    height: AppSize.s16,
+                  ),
+                  SMTextFConst(
+                    controller: typeController,
+                    keyboardType: TextInputType.text,
+                    text: 'Type',
+                  ),
+                  SizedBox(
+                    height: AppSize.s16,
+                  ),
+                  SMTextFConst(
+                    controller: shorthandController,
+                    keyboardType: TextInputType.streetAddress,
+                    text: 'Shorthand',
+                  ),
+                  SizedBox(
+                    height: AppSize.s16,
+                  ),
+                  SMTextFConst(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    text: 'Type of Employee',
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width / 27,
+                  ),
                   Center(
                     child: CustomElevatedButton(
-                        width: AppSize.s105,
-                        height: AppSize.s30,
-                        text: 'Save',
-                        onPressed: onSavePressed),
+                      width: AppSize.s105,
+                      height: AppSize.s30,
+                      text: 'Save',
+                      onPressed: onSavePressed,
+                    ),
                   )
                 ],
               ),
@@ -101,6 +168,146 @@ class EditPopupWidget extends StatelessWidget {
           ],
         ),
       ),
+    );
+
+    //   Dialog(
+    //   backgroundColor: Colors.transparent,
+    //   child: Container(
+    //     height: AppSize.s400,
+    //     width: AppSize.s350,
+    //     decoration: BoxDecoration(
+    //       color: ColorManager.white,
+    //       borderRadius: BorderRadius.circular(8),
+    //     ),
+    //     child: Column(
+    //       children: [
+    //         Row(
+    //           mainAxisAlignment: MainAxisAlignment.end,
+    //           children: [
+    //             IconButton(
+    //               onPressed: () {
+    //                 Navigator.pop(context);
+    //               },
+    //               icon: Icon(Icons.close),
+    //             ),
+    //           ],
+    //         ),
+    //         Padding(
+    //           padding: const EdgeInsets.symmetric(
+    //               vertical: AppPadding.p3, horizontal: AppPadding.p20),
+    //           child: Column(
+    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //             children: [
+    //               Row(
+    //                 children: [
+    //                   Text(
+    //                     'Color',
+    //                     style: GoogleFonts.firaSans(
+    //                       fontSize: 12,
+    //                       fontWeight: FontWeight.w700,
+    //                       color: Color(0xff686464),
+    //                       decoration: TextDecoration.none,
+    //                     ),
+    //                   ),
+    //                   SizedBox(
+    //                     width: AppSize.s25,
+    //                   ),
+    //                   Container(
+    //                     padding: EdgeInsets.all(2),
+    //                     width: 61,
+    //                     height: 22,
+    //                     decoration: BoxDecoration(
+    //                       borderRadius: BorderRadius.circular(2),
+    //                       border: Border.all(
+    //                         width: 1,
+    //                         color: Color(0xffE9E9E9),
+    //                       ),
+    //                     ),
+    //                     child: Container(
+    //                       width: 57,
+    //                       height: 16,
+    //                       decoration: BoxDecoration(
+    //                         borderRadius: BorderRadius.circular(2),
+    //                         color: containerColor,
+    //                       ),
+    //                     ),
+    //                   )
+    //                 ],
+    //               ),
+    //               SizedBox(
+    //                 height: AppSize.s16,
+    //               ),
+    //               SMTextFConst(
+    //                 controller: typeController,
+    //                 keyboardType: TextInputType.text,
+    //                 text: 'Type',
+    //               ),
+    //               SizedBox(
+    //                 height: AppSize.s16,
+    //               ),
+    //               SMTextFConst(
+    //                 controller: shorthandController,
+    //                 keyboardType: TextInputType.streetAddress,
+    //                 text: 'Shorthand',
+    //               ),
+    //               SizedBox(
+    //                 height: AppSize.s16,
+    //               ),
+    //               SMTextFConst(
+    //                 controller: emailController,
+    //                 keyboardType: TextInputType.emailAddress,
+    //                 text: 'Type of Employee',
+    //               ),
+    //               SizedBox(
+    //                 height: MediaQuery.of(context).size.width / 27,
+    //               ),
+    //               Center(
+    //                 child: CustomElevatedButton(
+    //                     width: AppSize.s105,
+    //                     height: AppSize.s30,
+    //                     text: 'Save',
+    //                     onPressed: onSavePressed),
+    //               )
+    //             ],
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+  }
+
+  void _openColorPicker(BuildContext context) {
+    Color selectedColor = containerColor;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Pick a color'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              borderColor: containerColor,
+              onColorChanged: (Color color) {
+                selectedColor = color; // Update selectedColor
+              },
+              // enableAlpha: false,
+              // displayThumbColor: true,
+              // showLabel: false,
+              // pickerAreaHeightPercent: 0.8,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Update the containerColor with the selectedColor
+                onColorChanged(selectedColor);
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
