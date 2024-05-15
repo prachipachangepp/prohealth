22694e673e7f71f3c0d36471678286b1c996fb45
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2024-05-15 09:58:49 +0530

    log in screen base updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:prohealth/presentation/screens/hr_module/manage/controller/controller.dart';
-import 'package:prohealth/presentation/widgets/profile_bar/dummy_login.dart';
+import 'package:prohealth/presentation/widgets/responsive_screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,15 +19,14 @@ class MyApp extends StatelessWidget {
         initialRoute: "/",
         home: // MenuScreen()
             //HrAdministrativeScreen()
-            DumLogInScreen()
-        // SMDesktop()
-        // MobileLogIn()
-        ///
-        //  ResponsiveScreenSM()
-        ///
-        //     ResponsivePage(
-        //   controller: myController,
-        // )
-        );
+            // DumLogInScreen()
+            // SMDesktop()
+            // MobileLogIn()
+            ///
+            //  ResponsiveScreenSM()
+            ///
+            ResponsivePage(
+          controller: myController,
+        ));
   }
 }
