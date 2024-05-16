61ee6407b294a4879b9d6dda9b04ddffb4787070
SaloniPardeshi <pardeshisaloni22@gmail.com>
2024-05-16 16:58:02 +0530

    created newPassScreen, verifyPassScreen, called api, solved bugs, remove renderflex error.

--- lib/app/resources/const_string.dart
+++ lib/app/resources/const_string.dart
@@ -164,10 +164,12 @@ class AppString {
   // static const String = "";
   /// change password
   static const String verification = 'Verification';
+  static const String newPass = 'New Password';
   static const String entersixdigitCode = 'Enter your 6 digits code that you received on your email.';
   static const String enterotp = 'Please enter OTP';
+  static const String fillfield = 'Please fill all fields.';
   static const String didntrecieveCode = 'Didn’t receive a code?';
-  static const String setnewPassword = 'Set the new password for your account \nso you can login and access all features.';
+  static const String setnewPassword = 'Set the new password for your account so you can login and\naccess all features.';
   static const String enternewpass = 'Enter New Password';
   static const String confmpass = 'Confirm Password';
   static const String enterconfmpass = 'Enter Confirm Password';
--- lib/presentation/widgets/login_screen/verify_screen.dart
+++ lib/presentation/widgets/login_screen/verify_screen.dart
@@ -111,7 +111,7 @@ class _VerifyScreenState extends State<VerifyScreen> {
                     4,
                         (index) => Container(
                       width: MediaQuery.of(context).size.width / 38,
-                      height: MediaQuery.of(context).size.width / 38,
+                      height: MediaQuery.of(context).size.height / 19,
                       margin: EdgeInsets.symmetric(horizontal: AppPadding.p10),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(2.26),
@@ -176,11 +176,6 @@ class _VerifyScreenState extends State<VerifyScreen> {
                           )
                       ),
                     )
-
-                    // recognizer: TapGestureRecognizer()
-                    //   ..onTap = () {
-                    //     print("Resend code tapped");
-                    //   },
                   ],
                 ),
                 ///button
