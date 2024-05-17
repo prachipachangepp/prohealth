ef889c1a292a3adba0121a158db4543bf9d7b324
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2024-05-17 11:06:33 +0530

    color picker and pagination const added

--- lib/presentation/screens/sm_module/hr_screens/hr_administrative.dart
+++ lib/presentation/screens/sm_module/hr_screens/hr_administrative.dart
@@ -2,12 +2,14 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:prohealth/app/resources/const_string.dart';
 import 'package:prohealth/presentation/screens/sm_module/hr_screens/widgets/add_emp_popup_const.dart';
 import 'package:prohealth/presentation/screens/sm_module/hr_screens/widgets/admin_emp_data.dart';
 import 'package:prohealth/presentation/screens/sm_module/hr_screens/widgets/edit_emp_popup_const.dart';
+import 'package:prohealth/presentation/widgets/profile_bar/widget/pagination_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../app/resources/color.dart';
-import '../../../../app/resources/font_manager.dart';
 import '../../../../app/resources/value_manager.dart';
 import '../../../widgets/custom_icon_button_constant.dart';
 import '../widgets/table_constant.dart';
@@ -31,7 +33,7 @@ class _HrAdministrativeScreenState extends State<HrAdministrativeScreen> {
   late int currentPage;
   late int itemsPerPage;
   late List<String> items;
-
+  late List<Color> containerColors;
   @override
   void initState() {
     super.initState();
@@ -39,6 +41,25 @@ class _HrAdministrativeScreenState extends State<HrAdministrativeScreen> {
     itemsPerPage = 6;
     items = List.generate(20, (index) => 'Item ${index + 1}');
     administrativeData.loadEmployeeData();
+    containerColors = List.generate(20, (index) => Color(0xffE8A87D));
+    _loadColors();
+  }
+
+  void _loadColors() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      for (int i = 0; i < containerColors.length; i++) {
+        int? colorValue = prefs.getInt('containerColor$i');
+        if (colorValue != null) {
+          containerColors[i] = Color(colorValue);
+        }
+      }
+    });
+  }
+
+  void _saveColor(int index, Color color) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('containerColor$index', color.value);
   }
 
   @override
@@ -51,7 +72,7 @@ class _HrAdministrativeScreenState extends State<HrAdministrativeScreen> {
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         CustomIconButtonConst(
-            text: 'Add Employee Type',
+            text: AppString.addemployeetype,
             icon: Icons.add,
             onPressed: () {
               showDialog(
@@ -71,12 +92,13 @@ class _HrAdministrativeScreenState extends State<HrAdministrativeScreen> {
           height: 20,
         ),
         TableHeadConstant(items: [
-          TableHeadItem(text: 'Sr No', textAlign: TextAlign.start),
-          TableHeadItem(text: 'EmployeeType', textAlign: TextAlign.start),
+          TableHeadItem(text: AppString.srNo, textAlign: TextAlign.start),
           TableHeadItem(
-              text: 'Abbreviation            ', textAlign: TextAlign.start),
-          TableHeadItem(text: 'Color', textAlign: TextAlign.start),
-          TableHeadItem(text: 'Actions ', textAlign: TextAlign.center),
+              text: AppString.employeetype, textAlign: TextAlign.start),
+          TableHeadItem(
+              text: AppString.abbrevation, textAlign: TextAlign.start),
+          TableHeadItem(text: AppString.color, textAlign: TextAlign.start),
+          TableHeadItem(text: AppString.action, textAlign: TextAlign.center),
         ]),
         SizedBox(
           height: 5,
@@ -151,14 +173,11 @@ class _HrAdministrativeScreenState extends State<HrAdministrativeScreen> {
                         ),
                         Expanded(
                           child: Container(
-                            alignment: Alignment.center,
-                            //width: MediaQuery.of(context).size.width/20,
-                            width: 64,
+                            width: MediaQuery.of(context).size.width / 20,
                             height: 22,
-                            // margin: EdgeInsets.only(right: 20),
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(8),
-                              color: Color(0xffE8A87D),
+                              color: containerColors[index],
                             ),
                           ),
                         ),
@@ -173,23 +192,55 @@ class _HrAdministrativeScreenState extends State<HrAdministrativeScreen> {
                                     context: context,
                                     builder: (BuildContext context) {
                                       return EditPopupWidget(
-                                        typeController: typeController,
+                                        typeController: TextEditingController(),
                                         shorthandController:
-                                            shorthandController,
-                                        emailController: emailController,
-                                        containerColor: Color(0xffF37F81),
+                                            TextEditingController(),
+                                        emailController:
+                                            TextEditingController(),
+                                        containerColor: containerColors[index],
                                         onSavePressed: () {},
-                                        onColorChanged: (Color) {},
-                                        // onColorChanged: (Color) {},
+                                        onColorChanged: (Color color) {
+                                          setState(() {
+                                            containerColors[index] = color;
+                                            _saveColor(index, color);
+                                          });
+                                        },
+                                        // onColorChanged: (Color color) {
+                                        //   setState(() {
+                                        //     containerColors[index] =
+                                        //      color; // Update color for this item
+                                        //   });
+                                        // },
                                       );
                                     },
                                   );
                                 },
-                                icon: Icon(
-                                  Icons.edit_outlined,
-                                  color: ColorManager.mediumgrey,
-                                ),
+                                icon: Icon(Icons.edit_outlined),
+                                color: ColorManager.mediumgrey,
                               ),
+                              // IconButton(
+                              //   onPressed: () {
+                              //     showDialog(
+                              //       context: context,
+                              //       builder: (BuildContext context) {
+                              //         return EditPopupWidget(
+                              //           typeController: typeController,
+                              //           shorthandController:
+                              //               shorthandController,
+                              //           emailController: emailController,
+                              //           containerColor: Color(0xffF37F81),
+                              //           onSavePressed: () {},
+                              //           onColorChanged: (Color) {},
+                              //           // onColorChanged: (Color) {},
+                              //         );
+                              //       },
+                              //     );
+                              //   },
+                              //   icon: Icon(
+                              //     Icons.edit_outlined,
+                              //     color: ColorManager.mediumgrey,
+                              //   ),
+                              // ),
                               IconButton(
                                 onPressed: () {},
                                 icon: Icon(
@@ -207,118 +258,143 @@ class _HrAdministrativeScreenState extends State<HrAdministrativeScreen> {
         SizedBox(
           height: 10,
         ),
-        Container(
-          height: 30,
-          // color: Colors.black12,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 20,
-                height: 20,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(6.39),
-                  border: Border.all(
-                    color: ColorManager.grey,
-                    width: 0.79,
-                  ),
-                ),
-                child: IconButton(
-                  padding: EdgeInsets.only(bottom: 1.5),
-                  icon: Icon(Icons.chevron_left),
-                  onPressed: () {
-                    setState(() {
-                      currentPage = currentPage > 1 ? currentPage - 1 : 1;
-                    });
-                  },
-                  color: ColorManager.black,
-                  iconSize: 20,
-                ),
-              ),
-              SizedBox(width: 3),
-              for (var i = 1; i <= (items.length / itemsPerPage).ceil(); i++)
-                if (i == 1 ||
-                    i == currentPage ||
-                    i == (items.length / itemsPerPage).ceil())
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        currentPage = i;
-                      });
-                    },
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      margin: EdgeInsets.only(left: 5, right: 5),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(4),
-                        border: Border.all(
-                          color: currentPage == i
-                              ? ColorManager.blueprime
-                              : ColorManager.grey,
-                          width: currentPage == i ? 2.0 : 1.0,
-                        ),
-                        color: currentPage == i
-                            ? ColorManager.blueprime
-                            : Colors.transparent,
-                        // border: Border.all(
-                        //   color: currentPage == i
-                        //       ? Colors.blue
-                        //       : Colors.transparent,
-                        // ),
-                      ),
-                      child: Text(
-                        '$i',
-                        style: TextStyle(
-                          color: currentPage == i ? Colors.white : Colors.grey,
-                          fontWeight: FontWeightManager.bold,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                  )
-                else if (i == currentPage - 1 || i == currentPage + 1)
-                  Text(
-                    '..',
-                    style: TextStyle(
-                      color: ColorManager.black,
-                      fontWeight: FontWeightManager.bold,
-                      fontSize: 12,
-                    ),
-                  ),
-              Container(
-                width: 20,
-                height: 20,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(4),
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 0.79,
-                  ),
-                ),
-                child: IconButton(
-                  padding: EdgeInsets.only(bottom: 2),
-                  icon: Icon(Icons.chevron_right),
-                  onPressed: () {
-                    setState(() {
-                      currentPage =
-                          currentPage < (items.length / itemsPerPage).ceil()
-                              ? currentPage + 1
-                              : (items.length / itemsPerPage).ceil();
-                    });
-                  },
-                  color: ColorManager.black,
-                  iconSize: 20,
-                ),
-              ),
-            ],
-          ),
-        )
+        PaginationControlsWidget(
+          currentPage: currentPage,
+          items: items,
+          itemsPerPage: itemsPerPage,
+          onPreviousPagePressed: () {
+            /// Handle previous page button press
+            setState(() {
+              currentPage = currentPage > 1 ? currentPage - 1 : 1;
+            });
+          },
+          onPageNumberPressed: (pageNumber) {
+            /// Handle page number tap
+            setState(() {
+              currentPage = pageNumber;
+            });
+          },
+          onNextPagePressed: () {
+            /// Handle next page button press
+            setState(() {
+              currentPage = currentPage < (items.length / itemsPerPage).ceil()
+                  ? currentPage + 1
+                  : (items.length / itemsPerPage).ceil();
+            });
+          },
+        ),
+        // Container(
+        //   height: 30,
+        //   // color: Colors.black12,
+        //   child: Row(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: <Widget>[
+        //       Container(
+        //         width: 20,
+        //         height: 20,
+        //         decoration: BoxDecoration(
+        //           shape: BoxShape.rectangle,
+        //           borderRadius: BorderRadius.circular(6.39),
+        //           border: Border.all(
+        //             color: ColorManager.grey,
+        //             width: 0.79,
+        //           ),
+        //         ),
+        //         child: IconButton(
+        //           padding: EdgeInsets.only(bottom: 1.5),
+        //           icon: Icon(Icons.chevron_left),
+        //           onPressed: () {
+        //             setState(() {
+        //               currentPage = currentPage > 1 ? currentPage - 1 : 1;
+        //             });
+        //           },
+        //           color: ColorManager.black,
+        //           iconSize: 20,
+        //         ),
+        //       ),
+        //       SizedBox(width: 3),
+        //       for (var i = 1; i <= (items.length / itemsPerPage).ceil(); i++)
+        //         if (i == 1 ||
+        //             i == currentPage ||
+        //             i == (items.length / itemsPerPage).ceil())
+        //           InkWell(
+        //             onTap: () {
+        //               setState(() {
+        //                 currentPage = i;
+        //               });
+        //             },
+        //             child: Container(
+        //               width: 20,
+        //               height: 20,
+        //               margin: EdgeInsets.only(left: 5, right: 5),
+        //               alignment: Alignment.center,
+        //               decoration: BoxDecoration(
+        //                 shape: BoxShape.rectangle,
+        //                 borderRadius: BorderRadius.circular(4),
+        //                 border: Border.all(
+        //                   color: currentPage == i
+        //                       ? ColorManager.blueprime
+        //                       : ColorManager.grey,
+        //                   width: currentPage == i ? 2.0 : 1.0,
+        //                 ),
+        //                 color: currentPage == i
+        //                     ? ColorManager.blueprime
+        //                     : Colors.transparent,
+        //                 // border: Border.all(
+        //                 //   color: currentPage == i
+        //                 //       ? Colors.blue
+        //                 //       : Colors.transparent,
+        //                 // ),
+        //               ),
+        //               child: Text(
+        //                 '$i',
+        //                 style: TextStyle(
+        //                   color: currentPage == i ? Colors.white : Colors.grey,
+        //                   fontWeight: FontWeightManager.bold,
+        //                   fontSize: 12,
+        //                 ),
+        //               ),
+        //             ),
+        //           )
+        //         else if (i == currentPage - 1 || i == currentPage + 1)
+        //           Text(
+        //             '..',
+        //             style: TextStyle(
+        //               color: ColorManager.black,
+        //               fontWeight: FontWeightManager.bold,
+        //               fontSize: 12,
+        //             ),
+        //           ),
+        //       Container(
+        //         width: 20,
+        //         height: 20,
+        //         alignment: Alignment.center,
+        //         decoration: BoxDecoration(
+        //           shape: BoxShape.rectangle,
+        //           borderRadius: BorderRadius.circular(4),
+        //           border: Border.all(
+        //             color: Colors.grey,
+        //             width: 0.79,
+        //           ),
+        //         ),
+        //         child: IconButton(
+        //           padding: EdgeInsets.only(bottom: 2),
+        //           icon: Icon(Icons.chevron_right),
+        //           onPressed: () {
+        //             setState(() {
+        //               currentPage =
+        //                   currentPage < (items.length / itemsPerPage).ceil()
+        //                       ? currentPage + 1
+        //                       : (items.length / itemsPerPage).ceil();
+        //             });
+        //           },
+        //           color: ColorManager.black,
+        //           iconSize: 20,
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // )
       ],
     );
   }
