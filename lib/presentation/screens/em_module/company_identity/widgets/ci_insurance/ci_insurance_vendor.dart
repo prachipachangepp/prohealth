5ee630990f6d3fcc1069001e211c9132dc0aa7a7
shubham <shubham.wani@binyuga.com>
2024-06-08 12:44:12 +0530

    Added container

--- lib/presentation/screens/em_module/company_identity/widgets/ci_insurance/ci_insurance_vendor.dart
+++ lib/presentation/screens/em_module/company_identity/widgets/ci_insurance/ci_insurance_vendor.dart
@@ -44,6 +44,9 @@ class _CiInsuranceVendorState extends State<CiInsuranceVendor> {
             });
 
           }),
+          SizedBox(
+            height: 15,
+          ),
           Container(
             height: 30,
             decoration: BoxDecoration(
