dbebccb37c8ced7e16a99d03c9650909ec06b02e
Prachi <pjtp1003@gmail.com>
2024-06-14 15:09:36 +0530

    orgdoc manager file

--- lib/app/services/api/managers/establishment_manager/ci_org_doc_manager.dart
+++ lib/app/services/api/managers/establishment_manager/ci_org_doc_manager.dart
@@ -3,6 +3,7 @@ import 'package:prohealth/app/services/api/api.dart';
 import 'package:prohealth/data/api_data/api_data.dart';
 import 'package:prohealth/data/api_data/establishment_data/company_identity/ci_org_document.dart';
 
+import '../../../../resources/const_string.dart';
 import '../../repository/establishment_manager/establishment_repository.dart';
 
 /// add corporate document POST
@@ -24,7 +25,53 @@ import '../../repository/establishment_manager/establishment_repository.dart';
 //
 //   }
 // }
-
+Future<ApiData> addCorporateDocumentPost(BuildContext context,
+    String name,
+    int docType,
+    int docSubType,
+    String docCreated,
+    String url,
+    String expiryType,
+    String expiryDate,
+    String expiryReminder,
+    String companyId,
+    String officeId,
+   ) async {
+  try {
+    var response = await Api(context)
+        .post(path: EstablishmentManagerRepository.addOrgDocumentPost(), data: {
+      "doc_name": name,
+      "document_type_id": 0,
+      "document_subtype_id": 0,
+      "doc_created_at": docCreated,
+      "url": url,
+      "expiry_type": expiryType,
+      "expiry_date": expiryDate,
+      "expiry_reminder": expiryReminder,
+      "company_id": companyId,
+       "office_id": officeId
+    });
+    print('::::$response');
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      print("Saved request");
+      return ApiData(
+          statusCode: response.statusCode!,
+          success: true,
+          message: response.statusMessage!);
+    } else {
+      print("Error 1");
+      return ApiData(
+          statusCode: response.statusCode!,
+          success: false,
+          message: response.data['message']);
+    }
+  } catch (e) {
+    print("Error $e");
+    print("Error 2");
+    return ApiData(
+        statusCode: 404, success: false, message: AppString.somethingWentWrong);
+  }
+}
 
 /// get
 Future<List<IdentityData>> getOrgDocfetch(BuildContext context, int companyId,
