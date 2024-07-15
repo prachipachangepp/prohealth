ef03023970ec909c787250db0461ae8f96696ab1
Bhavarthsonawane <Bhavarth.s@binyuga.com>
2024-07-15 15:09:11 +0530

    popup

--- lib/presentation/screens/hr_module/register/offer_letter_screen.dart
+++ lib/presentation/screens/hr_module/register/offer_letter_screen.dart
@@ -623,3 +623,342 @@ class _OfferLetterScreenState extends State<OfferLetterScreen> {
 }
 
 
+
+//
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:prohealth/app/resources/color.dart';
+// import 'package:prohealth/presentation/screens/hr_module/register/taxtfield_constant.dart';
+// import 'package:prohealth/presentation/screens/hr_module/register/widgets/dropdown_const.dart';
+// import 'package:prohealth/presentation/screens/hr_module/register/widgets/offer_letter_constant.dart';
+//
+// List<Map<String, dynamic>> checkboxData = [
+//   {'title': '95673', 'value': false},
+//   {'title': '95673', 'value': false},
+//   {'title': '95673', 'value': false},
+//   {'title': '95673', 'value': false},
+//   {'title': '95673', 'value': false},
+//   {'title': '56866', 'value': false},
+//   {'title': '56866', 'value': false},
+//   {'title': '56866', 'value': false},
+//   {'title': '56866', 'value': false},
+//   {'title': '56866', 'value': false},
+// ];
+//
+// List<Map<String, dynamic>> checkboxDataCity = [
+//   {'title': 'RioLinda', 'value': false},
+//   {'title': 'Antelope', 'value': false},
+//   {'title': 'PleasantGrove', 'value': false},
+//   {'title': 'Elverta', 'value': false},
+//   {'title': 'Natomas', 'value': false},
+//   {'title': 'Nicolaus', 'value': false},
+// ];
+//
+// class OfferLetterScreen extends StatefulWidget {
+//   const OfferLetterScreen({super.key});
+//
+//   @override
+//   State<OfferLetterScreen> createState() => _OfferLetterScreenState();
+// }
+//
+// class _OfferLetterScreenState extends State<OfferLetterScreen> {
+//   TextEditingController issueDateController = TextEditingController();
+//   TextEditingController lastDateController = TextEditingController();
+//   TextEditingController startDateController = TextEditingController();
+//   TextEditingController verbalAcceptanceController = TextEditingController();
+//   TextEditingController patientsController = TextEditingController();
+//
+//   String selectedDropdownValue = 'Per day';
+//   String dropdownValue = 'Salaried';
+//   late List<Map<String, dynamic>> checkboxStates;
+//   late List<Map<String, dynamic>> checkboxStatesCity;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     checkboxStates = List.from(checkboxData);
+//     checkboxStatesCity = List.from(checkboxDataCity);
+//   }
+//
+//   void handleDropdownChange(String? newValue) {
+//     setState(() {
+//       dropdownValue = newValue ?? 'Salaried';
+//     });
+//   }
+//
+//   List<Widget> _buildCheckboxes() {
+//     return checkboxStates.map((data) {
+//       return CheckboxListTile(
+//         title: Text(data['title'],
+//           style: GoogleFonts.firaSans(
+//               fontSize: 10.0,
+//               fontWeight: FontWeight.w500
+//           ),),
+//         value: data['value'],
+//         onChanged: (bool? value) {
+//           setState(() {
+//             data['value'] = value ?? false;
+//           });
+//         },
+//       );
+//     }).toList();
+//   }
+//
+//   List<Widget> _buildCheckboxesCity() {
+//     return checkboxStatesCity.map((data) {
+//       return CheckboxListTile(
+//         title: Text(data['title'],
+//           style: GoogleFonts.firaSans(
+//               fontSize: 10.0,
+//               fontWeight: FontWeight.w500
+//           ),),
+//         value: data['value'],
+//         onChanged: (bool? value) {
+//           setState(() {
+//             data['value'] = value ?? false;
+//           });
+//         },
+//       );
+//     }).toList();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//         automaticallyImplyLeading: false,
+//         title: Center(
+//           child: Text(
+//             'Offer Letter',
+//             style: GoogleFonts.firaSans(
+//                 fontSize: 12.0,
+//                 fontWeight: FontWeight.w500,
+//                 color: Color(0xff1696C8)),
+//           ),
+//         ),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             children: [
+//               Row(
+//                 children: [
+//                   Expanded(
+//                     child: CustomTextField(
+//                         controller: issueDateController,
+//                         labelText: 'Issue Date'),
+//                   ),
+//                   SizedBox(width: MediaQuery.of(context).size.width / 80),
+//                   Expanded(
+//                     child: CustomTextField(
+//                       controller: lastDateController,
+//                       labelText: 'Last Date',
+//                     ),
+//                   ),
+//                   SizedBox(width: MediaQuery.of(context).size.width / 80),
+//                   Expanded(
+//                     child: CustomTextField(
+//                       controller: startDateController,
+//                       labelText: 'Anticipated Start Date',
+//                     ),
+//                   )
+//                 ],
+//               ),
+//               SizedBox(height: MediaQuery.of(context).size.height / 60),
+//               Row(
+//                 children: [
+//                   Expanded(
+//                     child: CustomTextField(
+//                       controller: verbalAcceptanceController,
+//                       labelText: 'Verbal Acceptance',
+//                     ),
+//                   ),
+//                   SizedBox(width: MediaQuery.of(context).size.width / 80),
+//                   Expanded(
+//                     child: Stack(
+//                       children: [
+//                         TextField(
+//                           cursorColor: Colors.black,
+//                           controller: patientsController,
+//                           decoration: InputDecoration(
+//                             border: OutlineInputBorder(
+//                               borderSide: BorderSide(color: Color(0xffB1B1B1), width: 1.0),
+//                             ),
+//                             enabledBorder: OutlineInputBorder(
+//                               borderSide: BorderSide(color: Color(0xffB1B1B1), width: 1.0),
+//                             ),
+//                             focusedBorder: OutlineInputBorder(
+//                               borderSide: BorderSide(color: Color(0xffB1B1B1), width: 1.0),
+//                             ),
+//                             filled: true,
+//                             fillColor: Colors.white,
+//                             labelText: 'No. of Patients',
+//                             labelStyle: GoogleFonts.firaSans(
+//                                 fontSize: 10.0,
+//                                 fontWeight: FontWeight.w400,
+//                                 color: Color(0xff575757)),
+//                             suffixIcon: Container(
+//                               // padding: EdgeInsets.only(right: 10),
+//                               margin: EdgeInsets.only(right: 10, top: 6, bottom: 6),
+//                               height: 5,
+//                               decoration: BoxDecoration(
+//                                 border: Border.all(color: Colors.grey),
+//                                 borderRadius: BorderRadius.circular(5.0),
+//                               ),
+//                               child: DropdownButton<String>(
+//                                 value: selectedDropdownValue,
+//                                 items: ['Per day', 'Per week', 'Per month']
+//                                     .map((String value) {
+//                                   return DropdownMenuItem<String>(
+//                                     value: value,
+//                                     child: Container(
+//                                       padding: EdgeInsets.symmetric(
+//                                           horizontal: 12.0),
+//                                       child: Text(value),
+//                                     ),
+//                                   );
+//                                 }).toList(),
+//                                 onChanged: (String? value) {
+//                                   if (value != null) {
+//                                     setState(() {
+//                                       selectedDropdownValue = value;
+//                                     });
+//                                   }
+//                                 },
+//                                 underline: SizedBox(),
+//                                 icon: Icon(Icons.arrow_drop_down,
+//                                     color: ColorManager.blueprime),
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                   SizedBox(width: MediaQuery.of(context).size.width / 80),
+//                   SizedBox(width: MediaQuery.of(context).size.width / 3.15),
+//                 ],
+//               ),
+//               SizedBox(height: MediaQuery.of(context).size.height / 30),
+//               Divider(
+//                 color: ColorManager.cream,
+//                 thickness: 4,
+//               ),
+//               SizedBox(height: MediaQuery.of(context).size.height / 10),
+//               Container(
+//                 height: MediaQuery.of(context).size.height / 2,
+//                 decoration: BoxDecoration(
+//                   border: Border.all(color: Color(0xff1696C8)),
+//                   borderRadius: BorderRadius.circular(12.0),
+//                 ),
+//                 child: Padding(
+//                   padding: EdgeInsets.all(16.0),
+//                   child: Row(
+//                     children: [
+//                       Expanded(
+//                         child: Padding(
+//                           padding: EdgeInsets.only(left: 71.0),
+//                           child: Column(
+//                             children: [
+//                               CustomDropdownFormField(
+//                                   hintText: 'Select a City',
+//                                   labelText: 'City',
+//                                   items: [
+//                                     'ProHealth San Jose',
+//                                     'ProHealth Sacramento',
+//                                     'ProHealth Walnut Creek',
+//                                     'ProHealth Stockton',],
+//                                   onChanged: (String) {}
+//                               ),
+//                               SizedBox(height: MediaQuery.of(context).size.height / 30),
+//                               CustomDropdownFormField(
+//                                   hintText: 'Select a Country',
+//                                   labelText: 'Country',
+//                                   items: [
+//                                     'Alameida',
+//                                     'San Joachim'],
+//                                   onChanged: (String) {}
+//                               ),
+//                               SizedBox(height: MediaQuery.of(context).size.height / 30),
+//                               CustomDropdownFormField(
+//                                   hintText: 'Select a Zone',
+//                                   labelText: 'Zone',
+//                                   items: [
+//                                     '1',
+//                                     '2',
+//                                     '3',
+//                                     '4'],
+//                                   onChanged: (String) {}
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       ),
+//                       SizedBox(width: MediaQuery.of(context).size.width / 10),
+//                       Expanded(
+//                         child: DefaultTabController(
+//                           length: 2,
+//                           child: Column(
+//                             children: [
+//                               Container(
+//                                 child: TabBar(
+//                                   indicator: BoxDecoration(
+//                                       color: Color(0xff1696C8),
+//                                       borderRadius: BorderRadius.circular(20)),
+//                                   labelStyle: TextStyle(
+//                                       fontWeight: FontWeight.bold,
+//                                       fontSize: 14),
+//                                   labelColor: Colors.white,
+//                                   unselectedLabelColor: Colors.black,
+//                                   tabs: [
+//                                     Tab(
+//                                       child: Align(
+//                                         alignment: Alignment.center,
+//                                         child: Text('Zip Code'),
+//                                       ),
+//                                     ),
+//                                     Tab(
+//                                       child: Align(
+//                                         alignment: Alignment.center,
+//                                         child: Text('City'),
+//                                       ),
+//                                     ),
+//                                   ],
+//                                 ),
+//                               ),
+//                               Expanded(
+//                                 child: TabBarView(
+//                                   children: [
+//                                     SingleChildScrollView(
+//                                       scrollDirection: Axis.horizontal,
+//                                       child: Row(
+//                                         children: _buildCheckboxes(),
+//                                       ),
+//                                     ),
+//                                     SingleChildScrollView(
+//                                       scrollDirection: Axis.horizontal,
+//                                       child: Row(
+//                                         children: _buildCheckboxesCity(),
+//                                       ),
+//                                     ),
+//                                   ],
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
