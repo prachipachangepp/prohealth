ff955b614ec5f4d4021ab73e2207a319e5f24a6a
SaloniPardeshi1412 <139761063+SaloniPardeshi1412@users.noreply.github.com>
2024-07-19 10:20:44 +0530

    Merge bhavarth.

--- lib/presentation/screens/hr_module/onboarding/new_onboard_screen.dart
+++ lib/presentation/screens/hr_module/onboarding/new_onboard_screen.dart
@@ -95,8 +95,7 @@ class _OnboardingTabManageState extends State<OnboardingTabManage> {
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(20),
                               color: widget.selectedIndex == entry.key
-                                  ? Colors.white
-                                  : null,
+                                  ? Colors.white : null,
                             ),
                             child: Text(
                                 entry.value,
