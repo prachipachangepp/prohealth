5759a81beda6b7ddf2aab5635fc9a67e38c8a1ce
SaloniPardeshi1412 <139761063+SaloniPardeshi1412@users.noreply.github.com>
2024-07-19 23:03:08 +0530

    merge shubham.

--- lib/presentation/screens/hr_module/onboarding/widgets/genaral_tab.dart
+++ lib/presentation/screens/hr_module/onboarding/widgets/genaral_tab.dart
@@ -143,6 +143,7 @@ class _OnboardingGeneralState extends State<OnboardingGeneral> {
                                           //   child:
                                           //       Image.asset('images/profile.png'),
                                           // ),
+
                                           CircleAvatar(
                                             radius: MediaQuery.of(context).size.width / 50,
                                             backgroundImage: NetworkImage(snapshot.data![index].imgurl ?? 'images/profile.png'),
