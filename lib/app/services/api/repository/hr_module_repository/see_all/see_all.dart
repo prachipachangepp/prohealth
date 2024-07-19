32688141b26370efc41fefa7396082564652c85b
SaloniPardeshi1412 <139761063+SaloniPardeshi1412@users.noreply.github.com>
2024-07-19 09:30:58 +0530

    merge demo.

--- lib/app/services/api/repository/hr_module_repository/see_all/see_all.dart
+++ lib/app/services/api/repository/hr_module_repository/see_all/see_all.dart
@@ -2,7 +2,6 @@ class SeeAllRepository{
   static String emp = "/employees";
   static String all = "/all";
 
-
   ///employees/all/{companyId}
   static String getEmpSeeAll({required int compId}){
     return "$emp$all/$compId";
