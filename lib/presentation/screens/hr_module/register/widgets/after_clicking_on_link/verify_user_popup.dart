47ce95cf83ac74773eb31cef3cdf7652152c64fe
prajwalbin <prajwallok8@gmail.com>
2024-07-24 10:10:06 +0530

    verify popup

--- lib/presentation/screens/hr_module/register/widgets/after_clicking_on_link/verify_user_popup.dart
+++ lib/presentation/screens/hr_module/register/widgets/after_clicking_on_link/verify_user_popup.dart
@@ -38,7 +38,7 @@ class VerifyUserpopupState extends State<VerifyUserpopup> {
       backgroundColor: Colors.white,
       child: Container(
         width: MediaQuery.of(context).size.width * 0.3,
-        height:  MediaQuery.of(context).size.height*0.5,
+        height:  MediaQuery.of(context).size.height*0.4,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
