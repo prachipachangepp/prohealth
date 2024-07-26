9c12d0eebbb758f84e360cebce145fa8c7b5d1b8
shubham <shubham.wani@binyuga.com>
2024-07-26 19:31:54 +0530

    Timmer

--- lib/presentation/screens/hr_module/register/widgets/after_clicking_on_link/verify_user_popup.dart
+++ lib/presentation/screens/hr_module/register/widgets/after_clicking_on_link/verify_user_popup.dart
@@ -246,6 +246,7 @@ class VerifyUserpopupState extends State<VerifyUserpopup> {
                     ),
                     const SizedBox(height: 3),
                     otpEnabled ?
+                        _remainingTime > 0?
                       Align(
                         alignment: Alignment.centerRight,
                         child: Padding(
@@ -253,13 +254,16 @@ class VerifyUserpopupState extends State<VerifyUserpopup> {
                           child: Text(
                             '00:$_remainingTime',
                             style:  GoogleFonts.firaSans(
-                              fontSize: FontSize.s12,
+                              fontSize: FontSize.s13,
                               color: ColorManager.mediumgrey,
                               fontWeight: FontWeightManager.semiBold
                             ),
                           ),
                         ),
                       ): _remainingTime == 0 ?InkWell(
+                          splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
                       onTap: ()async{
                        await postverifyuser(context, emailController.text);
                         _remainingTime = 59;
@@ -280,7 +284,7 @@ class VerifyUserpopupState extends State<VerifyUserpopup> {
                         ),
                                             ),
                       ):
-                    const SizedBox(height: 20),
+                    const SizedBox(height: 20) : SizedBox(),
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         backgroundColor: const Color(0xFF50B5E5),
