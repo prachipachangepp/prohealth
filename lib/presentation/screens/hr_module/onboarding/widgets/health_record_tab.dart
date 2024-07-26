3e46721329abd44018feca52c1131b3d588b6e23
SaloniPardeshi1412 <139761063+SaloniPardeshi1412@users.noreply.github.com>
2024-07-26 10:46:26 +0530

    merge shubham.

--- lib/presentation/screens/hr_module/onboarding/widgets/health_record_tab.dart
+++ lib/presentation/screens/hr_module/onboarding/widgets/health_record_tab.dart
@@ -454,7 +454,6 @@ class _HealthRecordConstantState extends State<HealthRecordConstant> {
 
 
 
-
 ///without backend
 // Padding(
 //   padding:  EdgeInsets.only(right: MediaQuery.of(context).size.width/50),
