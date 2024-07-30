1ba1e7ad946c1e3da77f7ca7d7e8866245cc82d4
shubham <shubham.wani@binyuga.com>
2024-07-30 13:57:42 +0530

    Api integration on payrates

---
+++ lib/data/api_data/hr_module_data/manage/licenses_data.dart
@@ -0,0 +1,25 @@
+class LicensesData {
+  final String country;
+  final String documentType;
+  final int employeeID;
+  final String expDate;
+  final String issueDate;
+  final String licenseNumber;
+  final String licenseUrl;
+  final String licenseure;
+  final String org;
+  final String status;
+
+  LicensesData({
+    required this.country,
+    required this.documentType,
+    required this.employeeID,
+    required this.expDate,
+    required this.issueDate,
+    required this.licenseNumber,
+    required this.licenseUrl,
+    required this.licenseure,
+    required this.org,
+    required this.status,
+  });
+}
