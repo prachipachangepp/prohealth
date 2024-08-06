197d0f053f7c36687b1012a975da979f3aba9e6b
prajwalbin <prajwallok8@gmail.com>
2024-08-06 09:53:01 +0530

    merge

--- lib/presentation/screens/hr_module/register/register_screen.dart
+++ lib/presentation/screens/hr_module/register/register_screen.dart
@@ -346,7 +346,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           : TextButton(
                         onPressed: () async {
                           //html.window.open('/onBordingWelcome',"_blank");
-                          const url = "http://localhost:60734/#/onBordingWelcome";
+                          const url = "http://localhost:50308/#/onBordingWelcome";
                           //const url = "https://staging.symmetry.care/#/onBordingWelcome";
                           if (await canLaunch(url)) {
                             await launch(url);
