2b44abd1661abcd510f0cd82644ae80b37e04407
Prachi <pjtp1003@gmail.com>
2024-08-08 23:30:38 +0530

    Merge remote-tracking branch 'origin/prajwal_dev' into dev

--- lib/presentation/screens/hr_module/register/register_screen.dart
+++ lib/presentation/screens/hr_module/register/register_screen.dart
@@ -363,8 +363,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           : TextButton(
                         onPressed: () async {
                           //html.window.open('/onBordingWelcome',"_blank");
+
                           // const url = "http://localhost:62231/#/onBordingWelcome";
-                          const url = "https://staging.symmetry.care/#/onBordingWelcome";
+                           const url = "https://staging.symmetry.care/#/onBordingWelcome";
+                          //
+                          // const url = "http://localhost:62923/#/onBordingWelcome";
+                          //const url = "https://staging.symmetry.care/#/onBordingWelcome";
+
                           if (await canLaunch(url)) {
                             await launch(url);
                           //   Navigator.push(
