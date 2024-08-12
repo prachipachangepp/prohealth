1d110d40d26c0c0638e1f9a297ff6515937b82ac
prajwalbin <prajwallok8@gmail.com>
2024-08-12 21:21:44 +0530

    prifill get register screen

--- lib/presentation/screens/hr_module/register/register_screen.dart
+++ lib/presentation/screens/hr_module/register/register_screen.dart
@@ -364,19 +364,19 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           : TextButton(
                         onPressed: () async {
                           //html.window.open('/onBordingWelcome',"_blank");
-                          // const url = "http://localhost:60576/#/onBordingWelcome";
-                          // //const url = "https://staging.symmetry.care/#/onBordingWelcome";
-                          // if (await canLaunch(url)) {
-                           // await launch(url);
-                             Navigator.push(
-                               context,
-                               MaterialPageRoute(
-                                builder: (context) => OnBoardingWelcome(),
-                              ),
-                             );
-                           //} else {
-                          //   throw 'Could not launch $url';
-                          // }
+                          const url = "http://localhost:60576/#/onBordingWelcome";
+                          //const url = "https://staging.symmetry.care/#/onBordingWelcome";
+                          if (await canLaunch(url)) {
+                           await launch(url);
+                           //   Navigator.push(
+                           //     context,
+                           //     MaterialPageRoute(
+                           //      builder: (context) => OnBoardingWelcome(),
+                           //    ),
+                           //   );
+                           } else {
+                            throw 'Could not launch $url';
+                          }
                         },
                         child: Text(
                           data.link!,
