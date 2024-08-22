a00d559238d9ef2a35ae110cc7b5be3afc126a50
shubham <shubham.wani@binyuga.com>
2024-08-22 12:08:22 +0530

    Changes

--- lib/data/api_data/hr_module_data/offer_letter_html_data/offer_letter_html.dart
+++ lib/data/api_data/hr_module_data/offer_letter_html_data/offer_letter_html.dart
@@ -4,10 +4,12 @@ class OfferLetterData{
   final String docUploadStatus;
   final String templateName;
   final String template;
+  final int offerId;
   final int? statusCode;
 
   OfferLetterData( {
     this.statusCode,
+    required this.offerId,
     required this.docUploadStatus, required this.templateName, required this.template,
 
   });
