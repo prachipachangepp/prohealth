fec05a5943b341ff305f4de1d29e6a7c368215c1
prajwalbin <prajwallok8@gmail.com>
2024-08-23 10:06:50 +0530

    dd

--- lib/presentation/screens/hr_module/register/widgets/offer_letter_constant.dart
+++ lib/presentation/screens/hr_module/register/widgets/offer_letter_constant.dart
@@ -52,7 +52,7 @@ class _CustomTextFieldOfferScreenState extends State<CustomTextFieldOfferScreen>
               DateTime? pickedDate = await showDatePicker(
                 context: context,
                 initialDate: DateTime.now(),
-                firstDate: DateTime(2000),
+                firstDate: DateTime(1900),
                 lastDate: DateTime(2101),
               );
               if (pickedDate != null) {
