62e24a00781d2e760a4bf40f9d165062d1d3f75e
prajwalbin <prajwallok8@gmail.com>
2024-09-02 11:43:55 +0530

    em icon

--- lib/app/constants/app_config.dart
+++ lib/app/constants/app_config.dart
@@ -59,7 +59,7 @@ class AppConfig {
 
   static const String local = "";
   static const String prod = "";
-  static const String endpoint = demo;
+  static const String endpoint = dev;
 
   /// Document types
   static const int corporateAndCompliance = 8;
