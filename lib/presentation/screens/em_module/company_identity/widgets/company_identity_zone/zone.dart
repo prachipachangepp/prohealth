62045e8c0e63e5600931b2bf708dec6dd82610bc
prajwalbin <prajwallok8@gmail.com>
2024-09-03 19:58:14 +0530

    create user popup

--- lib/presentation/screens/em_module/company_identity/widgets/company_identity_zone/zone.dart
+++ lib/presentation/screens/em_module/company_identity/widgets/company_identity_zone/zone.dart
@@ -439,6 +439,7 @@ class _CiOrgDocumentState extends State<CiZone> {
                                               ),
                                             );
                                           }
+
                                           if (snapshotZone.hasData) {
                                             List dropDown = [];
                                             int docType = 0;
