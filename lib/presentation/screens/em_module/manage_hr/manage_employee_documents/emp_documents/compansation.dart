0501771c3d6e8b2aef4df247c0672dc0dd27504f
prajwalbin <prajwallok8@gmail.com>
2024-09-18 09:45:30 +0530

    merge

--- lib/presentation/screens/em_module/manage_hr/manage_employee_documents/emp_documents/compansation.dart
+++ lib/presentation/screens/em_module/manage_hr/manage_employee_documents/emp_documents/compansation.dart
@@ -208,23 +208,13 @@ class _CompansationEmpDocState extends State<CompansationEmpDoc> {
                                       child: Text(
                                         snapshot.data![index].docName.toString(),
                                         style:  DocumentTypeDataStyle.customTextStyle(context),
-                                        // style: GoogleFonts.firaSans(
-                                        //     fontSize: 10,
-                                        //     fontWeight: FontWeight.w700,
-                                        //     color: Color(0xff686464)
-                                        //   // color: isSelected ? Colors.white : Colors.black,
-                                        // ),
+
                                       )),
                                   Center(
                                       child: Text(
                                         snapshot.data![index].expiry.toString(),
                                         style:  DocumentTypeDataStyle.customTextStyle(context),
-                                        // style: GoogleFonts.firaSans(
-                                        //     fontSize: 10,
-                                        //     fontWeight: FontWeight.w700,
-                                        //     color: Color(0xff686464)
-                                        //   // color: isSelected ? Colors.white : Colors.black,
-                                        // ),
+
                                       )),
                                   Center(
                                       child: Text(
