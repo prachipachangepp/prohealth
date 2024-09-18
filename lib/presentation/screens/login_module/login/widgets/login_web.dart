23454a4483cd37722b2be7504d3c0398520958d6
Prachi <pjtp1003@gmail.com>
2024-09-18 15:23:04 +0530

    updated

--- lib/presentation/screens/login_module/login/widgets/login_web.dart
+++ lib/presentation/screens/login_module/login/widgets/login_web.dart
@@ -101,7 +101,7 @@ class _LoginWebState extends State<LoginWeb> {
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         const Text(
-                          "Version 1.0.3 (18) demo",
+                          "Version 1.0.3 (19) demo",
                           style: TextStyle(
                             fontSize: 10,
                           ),
