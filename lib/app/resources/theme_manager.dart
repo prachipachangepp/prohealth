971860547bb31bc64ee0d4c30a8712c8d85c8e32
SaloniPardeshi1412 <139761063+SaloniPardeshi1412@users.noreply.github.com>
2024-09-20 18:16:37 +0530

    Font Stylling Change in 1 files, remove google font.

--- lib/app/resources/theme_manager.dart
+++ lib/app/resources/theme_manager.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:prohealth/app/resources/font_manager.dart';
 
 import 'color.dart';
@@ -16,7 +15,7 @@ class TableHeadingStyle{
 
 
 class TableListTextStyle{
-  static final TextStyle firaSansGray10Bold = GoogleFonts.firaSans(
+  static final TextStyle firaSansGray10Bold = TextStyle(
     fontSize: 10,
     fontWeight: FontWeight.w600,
     color: Color(0xff686464),
