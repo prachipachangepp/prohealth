eb9dcf5a7e3f169648162c3d440827b24b22911d
prajwalbin <prajwallok8@gmail.com>
2024-09-25 18:31:33 +0530

    msg popup vender

--- lib/presentation/screens/em_module/company_identity/widgets/ci_insurance/widgets/custome_dialog.dart
+++ lib/presentation/screens/em_module/company_identity/widgets/ci_insurance/widgets/custome_dialog.dart
@@ -8,6 +8,7 @@ import '../../../../../../../app/resources/font_manager.dart';
 import '../../../../widgets/button_constant.dart';
 import '../../../../widgets/dialogue_template.dart';
 import '../../../../widgets/text_form_field_const.dart';
+import '../../whitelabelling/success_popup.dart';
 
 class CustomPopup extends StatefulWidget {
   final TextEditingController namecontroller;
@@ -95,6 +96,14 @@ class _CustomPopupState extends State<CustomPopup> {
             if (_isNameValid) {
               widget.onPressed(); // Call the onPressed if validation passes
               Navigator.pop(context);
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return CountySuccessPopup(
+                    message: 'Save Successfully',
+                  );
+                },
+              );
             }
           },
         ), title:widget.title,
