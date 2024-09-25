ba9f42c5387c2a8c304cf17ab5a17c08e4c7fb28
shubham <shubham.wani@binyuga.com>
2024-09-25 10:55:35 +0530

    Changes

--- lib/app/resources/value_manager.dart
+++ lib/app/resources/value_manager.dart
@@ -67,7 +67,7 @@ class AppSize {
   static const double s7 = 7.0;
   static const double s8 = 8.0;
   static const double s9 = 9.0;
-  static const double s10 = 10.0;
+  static const double s10 = 10;
   static const double s11 = 11.0;
   static const double s12 = 12.0;
   static const double s13 = 13.0;
