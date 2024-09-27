a5aaa9276e5e8939f40948dcbe2993393b320187
prajwalbin <prajwallok8@gmail.com>
2024-09-27 19:27:34 +0530

    services not complet

--- lib/presentation/screens/hr_module/register/register_screen.dart
+++ lib/presentation/screens/hr_module/register/register_screen.dart
@@ -412,8 +412,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           : TextButton(
                         onPressed: () async {
                           //html.window.open('/onBordingWelcome',"_blank");
-                          //  const url = "http://localhost:58043/#/onBordingWelcome";
-                           const url = "https://staging.symmetry.care/#/onBordingWelcome";
+                       const url = "http://localhost:53988/#/onBordingWelcome";
+                          // const url = "https://staging.symmetry.care/#/onBordingWelcome";
                           //const url = "https://staging.symmetry.care/#/onBordingWelcome";
                         ///////////
                           if (await canLaunch(url)) {
