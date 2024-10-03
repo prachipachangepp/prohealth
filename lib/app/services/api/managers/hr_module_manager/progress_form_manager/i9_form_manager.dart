8eee68f85a2ed646e0d6498b3c1de27aa475d72e
prajwalbin <prajwallok8@gmail.com>
2024-10-03 20:18:05 +0530

    profile editatior coverage comment and popup msg apply in formscreen and form i9

--- lib/app/services/api/managers/hr_module_manager/progress_form_manager/i9_form_manager.dart
+++ lib/app/services/api/managers/hr_module_manager/progress_form_manager/i9_form_manager.dart
@@ -6,6 +6,8 @@ import 'package:prohealth/app/services/base64/encode_decode_base64.dart';
 import 'package:prohealth/app/services/token/token_manager.dart';
 import 'package:prohealth/data/api_data/api_data.dart';
 
+import '../../../../../../presentation/screens/em_module/company_identity/widgets/whitelabelling/success_popup.dart';
+
 Future<ApiDataRegister> addEmployeeI9Form({
   required BuildContext context,
   required int employeeId,
@@ -65,9 +67,17 @@ Future<ApiDataRegister> addEmployeeI9Form({
     );
     if (response.statusCode == 200 || response.statusCode == 201) {
       print("i9 form completed");
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("I9 form saved"),backgroundColor: Colors.green,),
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AddSuccessPopup(
+            message: 'I9 form saved',
+          );
+        },
       );
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(content: Text("I9 form saved"),backgroundColor: Colors.green,),
+      // );
       // orgDocumentGet(context);
       return ApiDataRegister(
           statusCode: response.statusCode!,
