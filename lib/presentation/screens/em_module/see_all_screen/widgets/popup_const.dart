1686733b5cdd871f22f35375304c560001e163b7
Prachi <pjtp1003@gmail.com>
2024-10-16 17:53:04 +0530

    updated

--- lib/presentation/screens/em_module/see_all_screen/widgets/popup_const.dart
+++ lib/presentation/screens/em_module/see_all_screen/widgets/popup_const.dart
@@ -288,7 +288,12 @@ class _CustomDialogState extends State<CustomDialog> {
             print('${widget.lastNameController.text}');
             print('${widget.emailController.text}');
             print('${widget.passwordController.text}');
-
+            if (widget.passwordController.text.length < 6) {
+              setState(() {
+                _PasswordDocError = "Password must be longer than or equal to 6 characters";
+              });
+              return; // Stop further execution if password validation fails
+            }
             if (_isFormValid) {
               var response = await createUserPost(
                 context,
