444fb6c1ada53ed47c73882c05e69352c8fb20a7
Prachi <pjtp1003@gmail.com>
2024-10-21 09:59:23 +0530

    Merge remote-tracking branch 'origin/prajwal_dev_17_10' into dev_fontstyle

--- lib/presentation/screens/hr_module/register/register_screen.dart
+++ lib/presentation/screens/hr_module/register/register_screen.dart
@@ -487,8 +487,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           : TextButton(
                         onPressed: () async {
                           //html.window.open('/onBordingWelcome',"_blank");
-                          // const url = "http://localhost:50634/#/onBordingWelcome";
-                         const url = "${AppConfig.deployment}/#/onBordingWelcome";
+                          const url = "http://localhost:51230/#/onBordingWelcome";
+                         // const url = "${AppConfig.deployment}/#/onBordingWelcome";
                           //const url = "https://staging.symmetry.care/#/onBordingWelcome";
                           ///
                           Navigator.push(context, MaterialPageRoute(builder: (_)=>OnBoardingWelcome()));
