b6056a4399f4191f0b2b00fb81adf0878778e83e
Prachi <pjtp1003@gmail.com>
2024-10-29 13:09:38 +0530

    updated

--- lib/app/constants/app_config.dart
+++ lib/app/constants/app_config.dart
@@ -5,7 +5,7 @@ class AppConfig {
   static const String demo = "https://demoapp.symmetry.care";
   static const googleApiKey = "AIzaSyCw6mXOPCtbKn5i0bWcAcnfXCkb0y5G7Lg";
   static const String deployment = "https://prohealth.symmetry.care";
-  static const String version = "Version 1.0.9 (11) demo";
+  static const String version = "Version 1.0.9 (12) demo";
 
   ///
   static const String local = "";
