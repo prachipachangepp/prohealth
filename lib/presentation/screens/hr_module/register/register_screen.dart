2864da7ff7c7dd70e0ed3c1e2e27e611ac6cdb46
Prachi <pjtp1003@gmail.com>
2024-10-30 17:15:20 +0530

    updated

--- lib/presentation/screens/hr_module/register/register_screen.dart
+++ lib/presentation/screens/hr_module/register/register_screen.dart
@@ -545,7 +545,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           : TextButton(
                         onPressed: () async {
                           //html.window.open('/onBordingWelcome',"_blank");
-                         const url = "http://localhost:64583/#/onBordingWelcome";
+                         const url = "http://localhost:63635/#/onBordingWelcome";
                          //  const url = "${AppConfig.deployment}/#/onBordingWelcome";
                           //const url = "https://staging.symmetry.care/#/onBordingWelcome";
                           //Navigator.push(context, MaterialPageRoute(builder: (_)=>OnBoardingWelcome()));
