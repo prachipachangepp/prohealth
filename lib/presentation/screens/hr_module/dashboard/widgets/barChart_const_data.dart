fec39237090a5c022393a22e27c73f1e5234583a
shubham <shubham.wani@binyuga.com>
2024-11-18 11:35:05 +0530

    Cahnges

--- lib/presentation/screens/hr_module/dashboard/widgets/barChart_const_data.dart
+++ lib/presentation/screens/hr_module/dashboard/widgets/barChart_const_data.dart
@@ -2,3 +2,16 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:prohealth/app/resources/color.dart';
 
+class ChartData {
+  ChartData(this.x, this.y);
+  final int x;
+  final double? y;
+}
+
+class ComboChartData {
+  ComboChartData(this.x, this.y, this.y1,);
+  final String x;
+  final double? y;
+  final double? y1;
+
+}
