2e6ea8cef95aaca8f3aa26df530bc5a533152dfc
prajwalbin <prajwallok8@gmail.com>
2024-12-06 17:37:17 +0530

    insurance screen final ui

--- lib/presentation/screens/scheduler_model/sm_Intake/widgets/intake_physician_info/widget/physician_info_info/physician_info_info.dart
+++ lib/presentation/screens/scheduler_model/sm_Intake/widgets/intake_physician_info/widget/physician_info_info/physician_info_info.dart
@@ -446,69 +446,7 @@ class _RelatedPartiesScreenstate extends State<PhysicianInfoInfoScreen> {
                                   }, headText: 'City',
                                 );
 
-                                // return SizedBox(
-                                //   height: 27,
-                                //   child: DropdownButtonFormField<String>(
-                                //     decoration: InputDecoration(
-                                //       labelText: 'City',
-                                //       labelStyle: GoogleFonts.firaSans(
-                                //         fontSize: 10.0,
-                                //         fontWeight: FontWeight.w400,
-                                //         color: ColorManager.greylight,
-                                //       ),
-                                //       focusedBorder: OutlineInputBorder(
-                                //         borderSide: BorderSide(
-                                //             color: ColorManager
-                                //                 .containerBorderGrey),
-                                //       ),
-                                //       border: OutlineInputBorder(
-                                //         borderRadius:
-                                //         BorderRadius.circular(4.0),
-                                //         borderSide: const BorderSide(
-                                //             color: Colors.grey),
-                                //       ),
-                                //       contentPadding:
-                                //       const EdgeInsets.symmetric(
-                                //         //   //  vertical: 5,
-                                //           horizontal: 12),
-                                //     ),
-                                //     // value: selectedCountry,
-                                //     icon: Icon(
-                                //       Icons.arrow_drop_down,
-                                //       color: ColorManager.blueprime,
-                                //     ),
-                                //     iconSize: 24,
-                                //     elevation: 16,
-                                //     style: GoogleFonts.firaSans(
-                                //       fontSize: 10.0,
-                                //       fontWeight: FontWeight.w400,
-                                //       color: const Color(0xff686464),
-                                //     ),
-                                //
-                                //     onChanged: (newValue) {
-                                //       for (var a in snapshot.data!) {
-                                //         if (a.cityName == newValue) {
-                                //           selectedCityff = a.cityName!;
-                                //           //country = a
-                                //           // int? docType = a.companyOfficeID;
-                                //         }
-                                //       }
-                                //     },
-                                //     items: dropDownList.map((String value) {
-                                //       return DropdownMenuItem<String>(
-                                //         value: value,
-                                //         child: Text(
-                                //           value,
-                                //           style: GoogleFonts.firaSans(
-                                //             fontSize: 12,
-                                //             color: Color(0xff575757),
-                                //             fontWeight: FontWeight.w400,
-                                //           ),
-                                //         ),
-                                //       );
-                                //     }).toList(),
-                                //   ),
-                                // );
+
                               } else {
                                 return const Offstage();
                               }
