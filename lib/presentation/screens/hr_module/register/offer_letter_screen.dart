ab99de13376086ba00334bdacb9000056620dcc6
prajwalbin <prajwallok8@gmail.com>
2024-12-12 16:47:00 +0530

    new merge 12.0 bug fix offer_letter_screen.dart   other screen in scheduler tab

--- lib/presentation/screens/hr_module/register/offer_letter_screen.dart
+++ lib/presentation/screens/hr_module/register/offer_letter_screen.dart
@@ -1311,7 +1311,7 @@ class _DynamciContainerState extends State<DynamciContainer> {
                                     ),
                                   );
                                 } else if (snapshot.hasError) {
-                                  return const CustomDropdownTextField(
+                                  return  CustomDropdownTextField(
                                     hintText: 'Select County',
                                     headText: 'County',
                                     items: ['Error'],
