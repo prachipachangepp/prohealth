928230c4736eb5a13e204c9d7653b323d9616001
shubham <shubham.wani@binyuga.com>
2024-12-13 17:49:03 +0530

    Cahnges and all branch merge

--- lib/presentation/screens/scheduler_model/widgets/sm_desktop_screen.dart
+++ lib/presentation/screens/scheduler_model/widgets/sm_desktop_screen.dart
@@ -172,8 +172,8 @@ class SMDesktopScreen extends StatelessWidget {
                 physics: NeverScrollableScrollPhysics(),
                 children: [
                   SMDashboardScreen(),
-                 // IntakeMainScreen(),
-                 SMIntakeScreen(),
+                  IntakeMainScreen(),
+                 // SMIntakeScreen(),
                   NewSchedulerScreen(),
                   SmCalenderScreen(),
                   SmLiveViewMapScreen(),
